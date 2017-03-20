ffe5bb30581e3b173f63c42860effc0a38e771a2
Serge Martynov <serge.martynov@gmail.com>
2017-03-21 01:44:13 +0300

    Initial commit. Make working basic POM structure.

---
+++ src/main/java/ru/sergm/PicocmdbApplication.java
@@ -0,0 +1,12 @@
+package ru.sergm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicocmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicocmdbApplication.class, args);
+	}
+}
