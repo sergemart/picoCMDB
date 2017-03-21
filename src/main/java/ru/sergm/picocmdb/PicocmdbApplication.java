02c13fda20a68b31dff983c592fa556cf698b3cf
Serge Martynov <serge.martynov@gmail.com>
2017-03-21 22:56:27 +0300

    Add config for servlet container

--- src/main/java/ru/sergm/picocmdb/PicocmdbApplication.java
+++ src/main/java/ru/sergm/picocmdb/PicocmdbApplication.java
@@ -1,10 +1,26 @@
 package ru.sergm.picocmdb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-public class PicocmdbApplication {
+@ComponentScan
+@EnableAutoConfiguration
+public class PicocmdbApplication
+		//to run on Tomcat
+		extends SpringBootServletInitializer{
+
+	// to run on Tomcat
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PicocmdbApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PicocmdbApplication.class, args);
