7736bb9f4d1c43d2bfd84bc56f54d9a87a9632e8
Serge Martynov <serge.martynov@gmail.com>
2017-04-05 01:13:20 +0300

    Make DAO integration tests independent of existing database records.

--- src/test/java/ru/sergm/picocmdb/AbstractIntegrationTestSuite.java
+++ src/test/java/ru/sergm/picocmdb/AbstractIntegrationTestSuite.java
@@ -7,13 +7,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)		// start embedded tomcat on random free port
 public abstract class AbstractIntegrationTestSuite {
 
+	private static AtomicInteger counter = new AtomicInteger(0);
+
 	@Autowired
 	protected JdbcTemplate jdbcTemplate;
 
+
+	// to make unique entities' IDs to prevent constraint violation when test executed in parallel
+	protected String getSalt() {
+		return String.valueOf(counter.incrementAndGet());
+	}
 
 }
--- src/test/java/ru/sergm/picocmdb/dao/RoleDaoIT.java
+++ src/test/java/ru/sergm/picocmdb/dao/RoleDaoIT.java
@@ -1,36 +1,37 @@
 package ru.sergm.picocmdb.dao;
 
-import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.assertEquals;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import ru.sergm.picocmdb.AbstractIntegrationTestSuite;
 import ru.sergm.picocmdb.domain.Role;
 
-
+/**
+ * The test suite uses JDBC to prepare data for tests and to produce expected results.
+ */
 public class RoleDaoIT extends AbstractIntegrationTestSuite {
 
 	@Autowired
 	private RoleDao roleDao; // the CuT
 
-	
+
 	@Test
 	@Transactional
 	@Rollback
 	public void findAll_Finds_Correct_Number_Of_Entities() {
 		// GIVEN
-			// just in case if the table is empty
-		this.jdbcTemplate.execute("INSERT INTO role(id, description, is_system) VALUES ('dummy_role_fafcnoe_1', 'dummy description 1', true)");
-		this.jdbcTemplate.execute("INSERT INTO role(id, description, is_system) VALUES ('dummy_role_fafcnoe_2', 'dummy description 2', false)");
-		String sqlQuery = "SELECT COUNT(*) FROM role"; // table names are case-sensitive on case-sensitive filesystems
-		Long sqlCount = this.jdbcTemplate.queryForObject(sqlQuery, Long.class);
+		String entityId1 = "ROLE" + super.getSalt();
+		String entityId2 = "ROLE" + super.getSalt();
+			// just in case if the table is empty; (!) table names are case-sensitive on case-sensitive filesystems
+		this.jdbcTemplate.update("INSERT INTO role(id, description, is_system) VALUES (?, 'dummy description', true)", (Object[]) new String[]{entityId1});
+		this.jdbcTemplate.update("INSERT INTO role(id, description, is_system) VALUES (?, 'dummy description', false)", (Object[]) new String[]{entityId2});
+		Long sqlCount = this.jdbcTemplate.queryForObject("SELECT COUNT(*) FROM role", Long.class);
 		// WHEN
 		List<Role> daoResult = this.roleDao.findAll();
 		// THEN
@@ -43,18 +44,20 @@ public class RoleDaoIT extends AbstractIntegrationTestSuite {
 	@Rollback
 	public void findAll_Finds_All_Stored_Entities() {
 		// GIVEN
-			// just in case if the table is empty
-		this.jdbcTemplate.execute("INSERT INTO role(id, description, is_system) VALUES ('dummy_role_fafase_1', 'dummy description 1', true)");
-		this.jdbcTemplate.execute("INSERT INTO role(id, description, is_system) VALUES ('dummy_role_fafase_2', 'dummy description 2', false)");
-		String sqlQuery = "SELECT * FROM role"; // table names are case-sensitive on case-sensitive filesystems
-		List<Role> sqlResult = this.jdbcTemplate.query(sqlQuery, new Object[] {}, (rs, rowNum) -> {
-				// custom lambda RowMapper.mapRow(); Spring BeanPropertyRowMapper can not properly map 'is_system' field, due to its 'non-standard' name
-				Role role = new Role();
-				role.setId(rs.getString("id"));
-				role.setDescription(rs.getString("description"));
-				role.setSystem(rs.getBoolean("is_system"));
-				return role;
-			}
+		String entityId1 = "ROLE" + super.getSalt();
+		String entityId2 = "ROLE" + super.getSalt();
+			// just in case if the table is empty; (!) table names are case-sensitive on case-sensitive filesystems
+		this.jdbcTemplate.update("INSERT INTO role(id, description, is_system) VALUES (?, 'dummy description', true)", (Object[]) new String[]{entityId1});
+		this.jdbcTemplate.update("INSERT INTO role(id, description, is_system) VALUES (?, 'dummy description', false)", (Object[]) new String[]{entityId2});
+		List<Role> sqlResult = this.jdbcTemplate.query("SELECT * FROM role", new Object[] {},
+				// custom lambda implementation for RowMapper.mapRow(); Spring BeanPropertyRowMapper can not properly map 'is_system' field, due to its 'non-standard' name
+				(rs, rowNum) -> {
+					Role role = new Role();
+					role.setId(rs.getString("id"));
+					role.setDescription(rs.getString("description"));
+					role.setSystem(rs.getBoolean("is_system"));
+					return role;
+				}
 		);
 		// WHEN
 		List<Role> daoResult = this.roleDao.findAll();
@@ -68,11 +71,12 @@ public class RoleDaoIT extends AbstractIntegrationTestSuite {
 	@Rollback
 	public void findById_Finds_Entity() {
 		// GIVEN
-		this.jdbcTemplate.execute("INSERT INTO role(id, description, is_system) VALUES ('dummy_role_fbife_1', 'dummy description 1', true)");
-		String sqlQuery = "SELECT * FROM role WHERE (id = 'dummy_role_fbife_1')"; // table names are case-sensitive on case-sensitive filesystems
+		String entityId1 = "ROLE" + super.getSalt();
+		this.jdbcTemplate.update("INSERT INTO role(id, description, is_system) VALUES (?, 'dummy description', true)", (Object[]) new String[] {entityId1});
 		//List<Role> sqlResult = jdbcTemplate.query(sqlQuery, new Object[] {}, new BeanPropertyRowMapper<Role>(Role.class));
-		List<Role> sqlResult = this.jdbcTemplate.query(sqlQuery, new Object[] {}, (rs, rowNum) -> {
-					// custom lambda RowMapper.mapRow(); Spring BeanPropertyRowMapper can not properly map 'is_system' field, due to its 'non-standard' name
+		List<Role> sqlResult = this.jdbcTemplate.query("SELECT * FROM role WHERE (id = ?)", new String[] {entityId1},
+				// custom lambda implementation for RowMapper.mapRow(); Spring BeanPropertyRowMapper can not properly map 'is_system' field, due to its 'non-standard' name
+				(rs, rowNum) -> {
 					Role role = new Role();
 					role.setId(rs.getString("id"));
 					role.setDescription(rs.getString("description"));
@@ -81,7 +85,7 @@ public class RoleDaoIT extends AbstractIntegrationTestSuite {
 				}
 		);
 		// WHEN
-		Role daoResult = this.roleDao.findById("dummy_role_fbife_1");
+		Role daoResult = this.roleDao.findById(entityId1);
 		// THEN
 		assertEquals(sqlResult.get(0), daoResult); // uses overloaded Role.equals()
 	}
